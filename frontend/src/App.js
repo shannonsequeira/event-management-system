784f269807c6d3b6ac8cd033f34c27cec5d8d910
shannonsequeira <seqshannon02@gmail.com>
2024-08-04 22:21:42 +0530

    delete event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-//App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
@@ -14,7 +13,8 @@ import EventDetail from "./components/EventDetail";
 import UserProfilePage from "./pages/UserProfilePage";
 import EditProfilePage from "./pages/EditProfilePage";
 import AboutUs from "./pages/AboutUs";
-import EditEventPage from "./pages/EditEventPage"; // Import the EditEventPage component
+import EditEventPage from "./pages/EditEventPage"; 
+import DeleteEventPage from "./pages/DeleteEventPage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
@@ -31,10 +31,11 @@ const App = () => {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/create-event" element={<EventForm />} />
             <Route path="/event/:id" element={<EventDetail />} />
-            <Route path="/edit-event/:id" element={<EditEventPage />} /> {/* Add this route */}
+            <Route path="/edit-event/:id" element={<EditEventPage />} /> 
             <Route path="/profile" element={<UserProfilePage />} />
             <Route path="/edit-profile" element={<EditProfilePage />} />
-            <Route path="/about" element={<AboutUs />} />;
+            <Route path="/delete-event/:id" element={<DeleteEventPage />} />
+            <Route path="/about" element={<AboutUs />} />
           </Routes>
         </div>
       </Router>
--- frontend/src/pages/DeleteEventPage.js
+++ frontend/src/pages/DeleteEventPage.js
@@ -1,26 +1,30 @@
-// DeleteEventPage.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { deleteEvent } from '../actions/eventActions';
 
 const DeleteEventPage = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const history = useHistory();
+  const navigate = useNavigate();
+  const hasConfirmed = useRef(false);
 
   useEffect(() => {
-    const confirmDelete = window.confirm('Are you sure you want to delete this event?');
-    if (confirmDelete) {
-      dispatch(deleteEvent(id)).then(() => {
-        history.push('/');
-      }).catch(error => {
-        console.error('Error deleting event:', error);
-      });
-    } else {
-      history.push(`/event/${id}`);
+    if (!hasConfirmed.current) {
+      const confirmDelete = window.confirm('Are you sure you want to delete this event?');
+      hasConfirmed.current = true;
+
+      if (confirmDelete) {
+        dispatch(deleteEvent(id)).then(() => {
+          navigate('/events');
+        }).catch(error => {
+          console.error('Error deleting event:', error);
+        });
+      } else {
+        navigate(`/event/${id}`);
+      }
     }
-  }, [dispatch, id, history]);
+  }, [dispatch, id, navigate]);
 
   return (
     <div>
